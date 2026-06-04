057d00eaf9f14f963b9a2e1f42c39d87121ec84a
IshaPatel07 <patelisha8067@gmail.com>
2026-06-04 18:44:21 +0530

    ...

--- Text-Utils/src/components/TextUtils.jsx
+++ Text-Utils/src/components/TextUtils.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 
 export default function TextUtils() {
-  return;
-  <>
-    <div className="container">
-      <h2 className="text-center mt-3">Text Utils</h2>
-      <div className="form-floating my-4">
-        <textarea
-          className="form-control"
-          placeholder="Leave a comment here"
-          id="floatingTextarea2"
-          style={{ height: 100 }}
-          defaultValue={""}
-        />
-        <label htmlFor="floatingTextarea2">content</label>
+  return (
+    <>
+      <div className="container">
+        <h2 className="text-center mt-3">Text Utils</h2>
+        <div className="form-floating my-4">
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="floatingTextarea2"
+            style={{ height: 100 }}
+            defaultValue={""}
+          />
+          <label htmlFor="floatingTextarea2">content</label>
+        </div>
+        <hr />
+        <ul>
+          <li></li>
+        </ul>
       </div>
-    </div>
-  </>;
+    </>
+  );
 }
