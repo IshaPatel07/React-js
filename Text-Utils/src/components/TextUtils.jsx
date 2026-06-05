b6cf670f3fed88b2a15e6d35d6601738981d7926
IshaPatel07 <patelisha8067@gmail.com>
2026-06-05 17:49:51 +0530

    05/06/2026

--- Text-Utils/src/components/TextUtils.jsx
+++ Text-Utils/src/components/TextUtils.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextUtils() {
+  const [text, setText] = useState("");
+  const [time, setTime] = useState(0);
   return (
     <>
       <div className="container">
         <h2 className="text-center mt-3">Text Utils</h2>
         <div className="form-floating my-4">
           <textarea
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
             className="form-control"
             placeholder="Leave a comment here"
             id="floatingTextarea2"
@@ -16,11 +21,38 @@ export default function TextUtils() {
           <label htmlFor="floatingTextarea2">content</label>
         </div>
         <hr />
+        <p>{text}</p>
         <ul>
-          <li>
-            
-          </li>
+          <li>Total Lenth : {text.length}</li>
+          <li>Total Words Lenth : {text.split(" ").length}</li>
+          <li>Reading speed : {time.toFixed(1)} sec</li>
         </ul>
+        <div>
+          <button
+            onClick={() => {
+              setText(text.toUpperCase());
+            }}
+            className="btn btn-primary mt-4 mx-3"
+          >
+            Uppercase
+          </button>
+          <button
+            onClick={() => {
+              setText(text.toLowerCase());
+            }}
+            className="btn btn-primary mt-4 "
+          >
+            Lowercase
+          </button>
+          <button
+            onClick={() => {
+              setTime((text.split(" ").length / 150) * 60);
+            }}
+            className="btn btn-primary mt-4 mx-3"
+          >
+            Reeding speed
+          </button>
+        </div>
       </div>
     </>
   );
