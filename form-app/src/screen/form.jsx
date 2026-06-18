40a1233695d45aba0510a9566c8a4804afee9fcd
IshaPatel07 <patelisha8067@gmail.com>
2026-06-18 17:13:00 +0530

    18/06/2026

--- form-app/src/screen/form.jsx
+++ form-app/src/screen/form.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+  const [student, setStudent] = useState({ skill: [] });
+
+  const getName = (e) => {
+    setStudent({ ...student, name: e.target.value });
+  };
+  const getEmail = (e) => {
+    setStudent({ ...student, email: e.target.value });
+  };
+  const getPhone = (e) => {
+    setStudent({ ...student, phone: e.target.value });
+  };
+  const getGender = (e) => {
+    setStudent({ ...student, gender: e.target.value });
+  };
+  const getSkills = (e) => {
+    setStudent({ ...student, skill: [...student.skill, e.target.value] });
+  };
+  const getCity = (e) => {
+    setStudent({ ...student, city: e.target.value });
+  };
+  const getAddrest = (e) => {
+    setStudent({ ...student, addrest: e.target.value });
+  };
+
+  const handlesubbmit = () => {
+    console.log(student);
+  };
+
   return (
     <>
       <form>
@@ -8,27 +36,96 @@ export default function Form() {
           <h2 className="text-center">Student Register Form</h2>
           <hr />
           <div className="d-flex justify-content-center mt-3">
-            <input type="text" placeholder="Enter Your Name" />
+            <input
+              onChange={getName}
+              type="text"
+              placeholder="Enter Your Name"
+            />
           </div>
           <div className="d-flex justify-content-center mt-3">
-            <input type="email" placeholder="Enter Your Email    " />
+            <input
+              onChange={getEmail}
+              type="email"
+              placeholder="Enter Your Email    "
+            />
           </div>
           <div className="d-flex justify-content-center mt-3">
-            <input type="text" placeholder="Enter Your Phone" />
+            <input
+              onChange={getPhone}
+              type="number"
+              placeholder="Enter Your Phone"
+            />
           </div>
           <div className="d-flex justify-content-center m-3 fs-5">
             <span className="fs-4 me-3">Gender :</span>
-            <input className="" type="radio" name="gender" />
+            <input
+              onChange={getGender}
+              className=""
+              value={"male"}
+              type="radio"
+              name="gender"
+            />
             mail
-            <input className="" type="radio" name="gender" />
+            <input
+              onChange={getGender}
+              className=""
+              value={"female"}
+              type="radio"
+              name="gender"
+            />
             Femail
           </div>
-          <div className="d-flex justify-content-center m-3 fs-5">
-            <span className="fs-4 me-3">Skills :</span>
-            <input className="" type="checkbox" name="gender" />
-                HTML
-            <input className="" type="checkbox" name="gender" />
+          <div className="d-flex justify-content-center fs-5">
+            <span className="fs-4  ">Skills :</span>
+            <input
+              onChange={getSkills}
+              className=" m-2"
+              value={"HTML"}
+              type="checkbox"
+              name="gender"
+            />
+            HTML
+            <input
+              onChange={getSkills}
+              className="m-2"
+              value={"CSS"}
+              type="checkbox"
+              name="gender"
+            />
             CSS
+            <input
+              onChange={getSkills}
+              className="m-2"
+              value={"JAVASCRIPT"}
+              type="checkbox"
+              name="gender"
+            />
+            JS
+            <input
+              onChange={getSkills}
+              className="m-2"
+              value={"REACTJS"}
+              type="checkbox"
+              name="gender"
+            />
+            ReactJS
+          </div>
+          <div className="d-flex justify-content-center">
+            <span className="fs-4  ">City :</span>
+            <select onChange={getCity}>
+              <option value="Surat">Surat</option>
+              <option value="Ahmdabad">Ahmdabad</option>
+              <option value="baroda">baroda</option>
+              <option value="gadhinagar">Gandhinagar</option>
+            </select>
+          </div>
+          <div className="d-flex justify-content-center mt-3">
+            <textarea onChange={getAddrest} placeholder="Addrest"></textarea>
+          </div>
+          <div className="d-flex justify-content-center mt-3">
+            <button onClick={handlesubbmit} className="btn btn-primary">
+              Subbmit
+            </button>
           </div>
         </div>
       </form>
