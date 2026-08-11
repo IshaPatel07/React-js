c5e88139b37391ebcc498b4257255de870458e60
Isha patel <patelisha8067@gmail.com>
2026-08-11 23:47:55 +0530

    Update Navbar.jsx

--- Blog-app/src/components/Navbar.jsx
+++ Blog-app/src/components/Navbar.jsx
@@ -1,77 +1,66 @@
-// import React from 'react'
-
-// export default function Navbar() {
-//     return (
-//         <div className=' m-3 col-8 d-flex justify-content-end'><nav className="navbar bg-body-tertiary">
-//             <div className="container-fluid">
-//                 <form className="d-flex" role="search">
-//                     <input
-//                         className="form-control me-2"
-//                         type="search"
-//                         placeholder="Search"
-//                         aria-label="Search"
-//                     />
-//                     <>
-//                         {/* Example single danger button */}
-//                         <div className="btn-group">
-//                             <button
-//                                 type="button"
-//                                 className="btn btn-primary dropdown-toggle"
-//                                 data-bs-toggle="dropdown"
-//                                 aria-expanded="false"
-//                             >
-//                                 All categories
-//                             </button>
-//                             <ul className="dropdown-menu">
-//                                 <li>
-//                                     <a className="dropdown-item" href="#">
-//                                         Action
-//                                     </a>
-//                                 </li>
-//                                 <li>
-//                                     <a className="dropdown-item" href="#">
-//                                         Another action
-//                                     </a>
-//                                 </li>
-//                                 <li>
-//                                     <a className="dropdown-item" href="#">
-//                                         Something else here
-//                                     </a>
-//                                 </li>
-//                                 <li>
-//                                     <hr className="dropdown-divider" />
-//                                 </li>
-//                                 <li>
-//                                     <a className="dropdown-item" href="#">
-//                                         Separated link
-//                                     </a>
-//                                 </li>
-//                             </ul>
-//                         </div>
-//                     </>
-
-//                 </form>
-//             </div>
-//         </nav>
-//         </div>
-//     )
-// }
-
-
-import { useSelector } from "react-redux";
+import { Link, useLocation } from "react-router";
 
 function Navbar() {
-    const blogs = useSelector((state) => state.blog.blogs);
+    const location = useLocation();
 
     return (
-        <nav className="navbar d-flex justify-content-center">
-            <h2 className="mx-3">📝 Blog Management</h2>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+            <div className="container">
 
-            <div className="Total mx-5" >
-                Total Blogs : {blogs.length}
+                {/* Logo */}
+                <Link className="navbar-brand fw-bold fs-4" to="/">
+                    📝 Blog Management
+                </Link>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div
+                    className="collapse navbar-collapse justify-content-end"
+                    id="navbarNav"
+                >
+
+
+                    <div className="d-flex mt-3  justify-content-between">
+
+                        <div className="nav-item me-2">
+                            <Link
+                                to="/"
+                                className={`btn ${location.pathname === "/"
+                                    ? "btn-primary"
+                                    : "btn-outline-light"
+                                    }`}
+                            >
+                                Home
+                            </Link>
+                        </div>
+
+                        <div className="nav-item">
+                            <Link
+                                to="/add-blog"
+                                className={`btn ${location.pathname === "/add-blog"
+                                    ? "btn-success"
+                                    : "btn-outline-success"
+                                    }`}
+                            >
+                                + Add Blog
+                            </Link>
+                        </div>
+                    </div>
+
+
+                </div>
+
             </div>
-        </nav >
+        </nav>
     );
+    // 
 }
 
 export default Navbar;
