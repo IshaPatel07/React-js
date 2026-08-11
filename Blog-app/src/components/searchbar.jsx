6c03ac97b5614c5ab03960c1f6f3e9b1a310a103
Isha patel <patelisha8067@gmail.com>
2026-08-11 23:48:40 +0530

    Update searchbar.jsx

--- Blog-app/src/components/searchbar.jsx
+++ Blog-app/src/components/searchbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
-export default function searchbar() {
-    return (
-        <div>searchbar</div>
-    )
+function SearchBar({ search, setSearch }) {
+  return (
+    <div className="mb-4">
+      <input
+        type="text"
+        className="form-control form-control-lg"
+        placeholder="🔍 Search blog by title..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
 }
+
+export default SearchBar;
