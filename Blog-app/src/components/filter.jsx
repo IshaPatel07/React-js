3096f5461890ba9d62fb05b98ccefdf1ec410d23
Isha patel <patelisha8067@gmail.com>
2026-08-11 23:44:46 +0530

    Update Filter.jsx

--- Blog-app/src/components/filter.jsx
+++ Blog-app/src/components/filter.jsx
@@ -1,9 +1,51 @@
+function FilterBar({
+  category,
+  setCategory,
+  date,
+  setDate,
+  clearFilters,
+}) {
+  return (
+    <div className="row mb-4">
 
+      {/* Category Filter */}
+      <div className="col-md-5 mb-2">
+        <select
+          className="form-select"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">All Categories</option>
+          <option value="React">React</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="Redux Toolkit">Redux Toolkit</option>
+          <option value="Node JS">Node JS</option>
+          <option value="CSS">CSS</option>
+        </select>
+      </div>
 
-import React from 'react'
+      {/* Date Filter */}
+      <div className="col-md-5 mb-2">
+        <input
+          type="date"
+          className="form-control"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
 
-export default function filter() {
-    return (
-        <div>filter</div>
-    )
+      {/* Clear Button */}
+      <div className="col-md-2 mb-2">
+        <button
+          className="btn btn-secondary w-100"
+          onClick={clearFilters}
+        >
+          Clear
+        </button>
+      </div>
+
+    </div>
+  );
 }
+
+export default FilterBar;
